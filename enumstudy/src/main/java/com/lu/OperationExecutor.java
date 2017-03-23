ede1102be2f121de57fa252622ffb9437754780e
Luyongjia <289609515@qq.com>
2017-03-23 17:07:56 +0800

    1、删除注释

--- enumstudy/src/main/java/com/lu/OperationExecutor.java
+++ enumstudy/src/main/java/com/lu/OperationExecutor.java
@@ -4,14 +4,16 @@ import com.lu.operation.impl.BaseOperation;
 import com.lu.operation.impl.ExtensionOperations;
 
 public class OperationExecutor {
+    /**
+     * @param args {@literal a>b || a==b}
+     */
     public static void main(String[] args) {
         for (BaseOperation baseOperation : BaseOperation.values()) {
-            baseOperation.apply(1,2);
+            baseOperation.apply(1.42,2);
         }
         for (ExtensionOperations extensionOperations : ExtensionOperations.values()) {
             extensionOperations.apply(5,4);
         }
     }
-
 
 }
